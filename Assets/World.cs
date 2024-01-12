7d0ed6d83fef34283fb89e695b61ac1d62249ab6
wilsonchenghy <150957380+wilsonchenghy@users.noreply.github.com>
2024-01-12 20:48:34 +0800

    update3 (3)

--- Assets/World.cs
+++ Assets/World.cs
@@ -15,35 +15,4 @@ public class Blocktype
 {
     public string blockName;
     public bool isSolid;
-
-    //[Header("Texture Values")]
-    //public int bottomFaceTexture;
-    //public int leftFaceTexture;
-    //public int backFaceTexture;
-    //public int rightFaceTexture;
-    //public int frontFaceTexture;
-    //public int topFaceTexture;
-
-    //public int getTextureID(int faceIndex)
-    //{
-    //    switch (faceIndex)
-    //    {
-
-    //        case 0:
-    //            return backFaceTexture;
-    //        case 1:
-    //            return frontFaceTexture;
-    //        case 2:
-    //            return topFaceTexture;
-    //        case 3:
-    //            return bottomFaceTexture;
-    //        case 4:
-    //            return leftFaceTexture;
-    //        case 5:
-    //            return rightFaceTexture;
-    //        default:
-    //            Debug.Log("Error in GetTextureID");
-    //            return 0;
-    //    }
-    //}
 }
